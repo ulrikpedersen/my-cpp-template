773172e16f7a7031813d38d999502843832a6e9d
Ulrik Pedersen <ulrikkofoed@gmail.com>
2022-07-04 21:06:59 +0100

    unimportant tweaks

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ int main(int argc, const char **argv)
 {
   cout << "Hello World!" << endl;
   int result = sum_int(1, 2);
+  int plus_one_never_used = result + 1;
   cout << "1 + 2 = " << result << endl;
   return 0;
 }
