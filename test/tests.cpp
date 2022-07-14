c5fa02de52148dd0a71d0d59532a6e42b1ecc063
Ulrik Pedersen <ulrikkofoed@gmail.com>
2022-07-14 21:47:59 +0100

    Add library unittest and CI reporting of results

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include "sum.hpp"
+
 unsigned int Factorial(unsigned int number)// NOLINT(misc-no-recursion)
 {
   return number <= 1 ? number : Factorial(number - 1) * number;
@@ -11,4 +13,10 @@ TEST_CASE("Factorials are computed", "[factorial]")
   REQUIRE(Factorial(2) == 2);
   REQUIRE(Factorial(3) == 6);
   REQUIRE(Factorial(10) == 3628800);
+}
+
+TEST_CASE("SumsCalculated", "[sum]")
+{
+  REQUIRE(sum_int(1, 2) == 3);
+  REQUIRE(sum_int(1, 2) == 4); // this should fail
 }
